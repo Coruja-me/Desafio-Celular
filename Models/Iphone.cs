93850d0f11b351750f37990e5c743338c00f2d69
Coruja <cauavalves@gmail.com>
2024-09-25 17:13:17 -0300

    update "TODO removido"

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,6 +1,5 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
         public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
@@ -8,7 +7,6 @@ namespace DesafioPOO.Models
 
         }
 
-        // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Seu iPhone instalou o aplicativo {nomeApp} com sucesso!");
