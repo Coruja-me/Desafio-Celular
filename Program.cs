05f372575048b7b92ceb896852db77d369c970e3
Coruja <cauavalves@gmail.com>
2024-09-25 17:11:17 -0300

    update "Concluindo o Desafio"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(
+    numero: "11912345678",
+    modelo: "Modelo 456",
+    imei: "111",
+    memoria: 64
+);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+
+
+Console.WriteLine("\n"+"iPhone:");
+Smartphone iphone = new Iphone(
+    numero: "11987564321",
+    modelo: "Modelo 16",
+    imei: "222",
+    memoria: 128
+);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
